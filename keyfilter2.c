6dd8af4b9f687e84fc6ef6fcd5fbb230a2375120
Adam <nezbeda.adam@gmail.com>
2023-10-27 21:05:35 +0200

    Not working redoing functions

--- keyfilter2.c
+++ keyfilter2.c
@@ -20,103 +20,123 @@ void stringToupper(char *str)
         str[i] = toupper(str[i]);
     }
 }
-char query_next_char(char address[LINE_LENGTH], char *input){
+char query_next_char(char *address, char *input)
+{
 
-    //Change to UPPERCASE for desired output
+    // Change to UPPERCASE for desired output
     stringToupper(address);
     stringToupper(input);
 
-    // Readability
-    int inputLength = (int)strlen(input);
-    int addressLength = (int)strlen(address);
-
-    if(inputLength > addressLength){
+    // Address == input
+    // FOUND
+    if (strcmp(input, address))
+    {
         return CHAR_MAX;
     }
-
-    bool sameLength = inputLength == addressLength ? true : false;
-    int upperBound = inputLength < addressLength ? inputLength : inputLength-1; 
-
-    for(int i = 0; i <= upperBound; i++)
+    int pos = 0;
+    while (pos < (int)strlen(address))
     {
-        // Returns the first dissimilar UPPERCASE char from string address based on input
-        if(input[i] != address[i] && (i != 0 && input[i-1] == address[i-1]))
+        if (input[pos] != address[pos])
         {
-            return address[i];
-
-        } else if(i == 0 && input == " "){
-
-            return address[i];
+            return address[pos];
         }
+        pos++;
     }
+    // int upperBound = inputLength < addressLength ? inputLength : inputLength-1;
 
-    // Address == input
-    if(sameLength){
-        return CHAR_MIN;
-    }
+    // for(int i = 0; i <= upperBound; i++)
+    //{
+         // Returns the first dissimilar UPPERCASE char from string address based on input
+    //     if(input[i] != address[i] && (i != 0 && input[i-1] == address[i-1]))
+    //     {
+    //         return address[i];
 
-    return 0;
+    //    } else if(i == 0 && input == " "){
+
+    //        return address[i];
+    //    } else{
+
+    //        return CHAR_MIN;
+    //    }
+    //}
+
+    return CHAR_MIN;
 }
 
-
-char* check_args(char input[], int argc)
+char *check_args(char input[], int argc)
 {
     if (argc > 2)
     {
         printf("Too many arguments!\n");
-        return CHAR_MIN;
-    } else if (argc < 2){
+        return NULL;
+    }
+    else if (argc < 2)
+    {
         return " ";
-    } else {
-        if (strlen(input) > LINE_LENGTH)
+    }
+    else
+    {
+        if ((int)strlen(input) > LINE_LENGTH)
         {
-            return CHAR_MIN;
+            printf("Argument is too long!\n");
+            return NULL;
         }
         return input;
-
     }
 }
 int main(int argc, char *argv[])
 {
     char *input = check_args(argv[1], argc);
-    if(input == CHAR_MIN)
+    if (input == NULL)
     {
         return EXIT_FAILURE;
     }
+    printf("%s\n", input);
 
-    // Array representing which ascii characters are included in the result 
+    // Array representing which ascii characters are included in the result
     bool includesChar[CHAR_SET_SIZE];
-    for(int i = 0; i < CHAR_SET_SIZE; i++){
+    for (int i = 0; i < CHAR_SET_SIZE; i++)
+    {
         includesChar[i] = false;
     }
 
-    char addresses[42][LINE_LENGTH]; // TODO: mb try *adresy[LINE_LENGTH] and then sizeof(blabla)
 
     char temp[100];
     char result;
+    bool equals;
 
-    while(1)
+    while (1)
     {
-        if(fgets(temp, sizeof(temp), stdin) == NULL){
+        equals = false;
+        if (fgets(temp, sizeof(temp), stdin) == NULL)
+        {
             break;
         }
-        
+
         // Remove endline at each string
         temp[strcspn(temp, "\n")] = 0;
         result = query_next_char(temp, input);
-        includesChar[(int)result] = true;
+        if (result != CHAR_MIN)
+        {
+            includesChar[(int)result] = true;
+        }
+        else if (result == CHAR_MAX)
+        {
+            equals = true;
+        }
         printf("%c\n", result);
     }
 
+    // kdyz je to jen o jedno pismenko vypsat found: NAZEV
     char enableChar;
     printf("Enable: ");
-    for(int i = 0; i < CHAR_SET_SIZE; i++){
-        if(includesChar[i]){
+    for (int i = 0; i < CHAR_SET_SIZE; i++)
+    {
+        if (includesChar[i])
+        {
             enableChar = (char)i;
             printf("%c", enableChar);
         }
     }
     printf("\n");
-
-
 }
